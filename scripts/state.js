865cf24c07aa4c61a9f5ce21301b4f3819d7bf15
pasha <p@korshunov.me>
2025-05-12 01:27:27 +0500

    moya vetochka

--- scripts/state.js
+++ scripts/state.js
@@ -3,55 +3,54 @@
 import { StringCalculator } from "./integrate.js";
 
 export class State {
-    // # - это приватное поле. Костыльный язык xD
-    static #p = 1;
-    static #T0 = 9;
-    static #left = 0;
-    static #right = 1;
-    static #dx = 0.0001;
-    static #pointsCount = 200;
-    static #modes = 100;
-    static #timeScale = 0.1;
-    static #isFrozen = false;
-    static #actualTime = 0;
-    static #startTime = 0;
+    static p = 1;
+    static T0 = 9;
+    static left = 0;
+    static right = 1;
+    static dx = 0.0001;
+    static pointsCount = 200;
+    static modes = 100;
+    static timeScale = 0.1;
+    static isFrozen = false;
+    static actualTime = 0;
+    static startTime = 0;
 
-    static #positionFunction = x => Math.sin(2 * Math.PI * x);
-    static #speedFunction    = x => 0;
+    static positionFunction = x => Math.sin(2 * Math.PI * x);
+    static speedFunction    = x => 0;
 
-    static #stringFunction = undefined;
-    static #a = 1;
+    static stringFunction = undefined;
+    static a = 1;
 
     static rebuild() {
-        State.#a = Math.sqrt(State.#T0 / State.#p);
-        State.#stringFunction = StringCalculator.getMainStringFunction(
-            State.#positionFunction, State.#speedFunction, State.#left, State.#right, State.#a, State.#dx, State.#modes
+        State.a = Math.sqrt(State.T0 / State.p);
+        State.stringFunction = StringCalculator.getMainStringFunction(
+            State.positionFunction, State.speedFunction, State.left, State.right, State.a, State.dx, State.modes
         );
     }
 
-    static getStringFunction () { return State.#stringFunction; }
-    static getBounds         () { return { left: State.#left, right: State.#right, bottom: -13, top: 13 }; }
-    static getPointsCount    () { return State.#pointsCount; }
-    static getStartTime      () { return State.#startTime; }
-    static getCurrentTime    () { return State.#actualTime; }
-    static isFrozenState     () { return State.#isFrozen; }
+    static getStringFunction () { return State.stringFunction; }
+    static getBounds         () { return { left: State.left, right: State.right, bottom: -1, top: 1 }; }
+    static getPointsCount    () { return State.pointsCount; }
+    static getStartTime      () { return State.startTime; }
+    static getCurrentTime    () { return State.actualTime; }
+    static isFrozenState     () { return State.isFrozen; }
 
-    static advanceTime  (dt) { State.#actualTime += dt * State.#timeScale; }
-    static resetTime    () { State.#actualTime = State.#startTime; }
+    static advanceTime  (dt) { State.actualTime += dt * State.timeScale; }
+    static resetTime    () { State.actualTime = State.startTime; }
 
 
-    static setDensity          (newP)  {  State.#p = newP; };
-    static setTension          (newT0) {  State.#T0 = newT0; };
-    static setBounds           (l, r) {  State.#left = l;  State.#right = r; };
-    static setPositionFunction (f) {  State.#positionFunction = f; }
-    static setSpeedFunction    (f) {  State.#speedFunction  = f; }
-    static setDx       (newDx) {  State.#dx = newDx;  State.rebuild(); }
-    static setModes    (newModes) {  State.#modes = newModes;  State.rebuild(); }
+    static setDensity          (newP)  {  State.p = newP; };
+    static setTension          (newT0) {  State.T0 = newT0; };
+    static setBounds           (l, r) {  State.left = l;  State.right = r; };
+    static setPositionFunction (f) {  State.positionFunction = f; }
+    static setSpeedFunction    (f) {  State.speedFunction  = f; }
+    static setDx       (newDx) {  State.dx = newDx;  State.rebuild(); }
+    static setModes    (newModes) {  State.modes = newModes;  State.rebuild(); }
 
-    static setPointsCount (newPointsCount) { State.#pointsCount = newPointsCount; }
-    static setTimeScale   (newTimeScale) {  State.#timeScale   = newTimeScale; }
-    static setStartTime   (newStartTime) {  State.#startTime =  State.#actualTime = newStartTime; }
-    static toggleFrozen   (newState) {  State.#isFrozen = newState; }
+    static setPointsCount (newPointsCount) { State.pointsCount = newPointsCount; }
+    static setTimeScale   (newTimeScale) {  State.timeScale   = newTimeScale; }
+    static setStartTime   (newStartTime) {  State.startTime =  State.actualTime = newStartTime; }
+    static toggleFrozen   (newState) {  State.isFrozen = newState; }
 
 }
 
